b607f6d91f8eb802cced45f9299f7837b7580ded
Facundo Campi <facucampi@gmail.com>
2018-11-08 17:13:26 -0300

    added models and POST /todos

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose.js');
+var {Users} = require('./models/users');
+var {Todo} = require('./models/todo');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) =>{
+        res.status(400).send(e);
+    })
+
+});
+
+app.listen(3000, () => console.log('server started in port 3000'));
+
+
